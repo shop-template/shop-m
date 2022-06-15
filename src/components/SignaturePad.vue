fd9d9ccf4710cd3fcd8f1e6f1edb7754390a29f2
fanyushun <fanyushun@zj.tech>
2022-06-15 22:28:22 +0800

    feat: 签字板增加工具栏、签字板演示

--- src/components/SignaturePad.vue
+++ src/components/SignaturePad.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="signature-pad">
-    <canvas class="signature-pad-canvas" ref="signaturePadCanvas" style="touch-action: none; user-select: none;"></canvas>
+    <div v-if="props.toolShow" class="signature-pad-header">
+      <div class="color-box">
+        <div v-for="item in props.colors" :key="item" class="color-item" :class="{ active: item === colorActive }" @click="changeColor(item)">
+          <div class="color-item-inner" :style="{ background: item }"></div>
+        </div>
+      </div>
+      <div class="handle-box">
+        <div class="handle-item" @click="clearCanvas">
+          <van-icon class="handle-item-icon" name="delete-o" />
+        </div>
+      </div>
+    </div>
+    <div class="signature-pad-canvas-box">
+      <canvas class="signature-pad-canvas" ref="signaturePadCanvas" style="touch-action: none; user-select: none;"></canvas>
+    </div>
   </div>
 </template>
 
@@ -13,9 +27,27 @@ import trimCanvas from 'trim-canvas'
 import { download } from '@/utils'
 
 const props = defineProps({
+  // 色组
+  colors: {
+    type: Array,
+    default() {
+      return ['#000', '#f00']
+    }
+  },
+  // 是否需要纯签名（去除周边留白）
+  isTrimCanvas: {
+    type: Boolean,
+    default: true
+  },
+  // 下载图片的名称
   downloadName: {
     type: String,
     default: 'signature'
+  },
+  // 是否展示上方工具栏颜色切换、清除签名
+  toolShow: {
+    type: Boolean,
+    default: true
   }
 })
 
@@ -25,23 +57,25 @@ defineExpose({
   getSignaturePad,
   clear: clearCanvas,
   isEmpty: isEmptyCanvas,
-  changePenColoe,
+  changePenColor,
   fromDataURL,
   toDataURL,
   fromData,
   toData,
   on: signaturePadOn,
   off: signaturePadOff,
+  saveImage,
   saveAsPng,
   saveAsJpg
 })
 
+const colorActive = ref(props.colors[0])
 const signaturePadCanvas = ref(null)
 let signaturePad = null
 onMounted(() => {
   nextTick(() => {
     signaturePad = new SignaturePad(signaturePadCanvas.value, {
-      backgroundColor: 'rgba(255,255,255,0)'
+      backgroundColor: props.isTrimCanvas ? 'rgba(255,255,255,0)' : 'rgb(255,255,255)'
     })
     resizeCanvas()
   })
@@ -62,11 +96,16 @@ function getCanvas() {
 }
 
 function getTrimmedCanvas() {
-  let copy = document.createElement('canvas')
-  copy.width = signaturePadCanvas.value.width
-  copy.height = signaturePadCanvas.value.height
-  copy.getContext('2d').drawImage(signaturePadCanvas.value, 0, 0)
-  return trimCanvas(copy)
+  if (signaturePad.isEmpty()) {
+    Toast('请先提供签名')
+    return false
+  } else {
+    let copy = document.createElement('canvas')
+    copy.width = signaturePadCanvas.value.width
+    copy.height = signaturePadCanvas.value.height
+    copy.getContext('2d').drawImage(signaturePadCanvas.value, 0, 0)
+    return trimCanvas(copy)
+  }
 }
 
 function getSignaturePad() {
@@ -84,7 +123,7 @@ function isEmptyCanvas() {
 }
 
 // 更改画笔颜色
-function changePenColoe(color) {
+function changePenColor(color) {
   signaturePad.penColor = color
 }
 
@@ -112,32 +151,81 @@ function signaturePadOff() {
   return signaturePad.off()
 }
 
+function changeColor(color) {
+  colorActive.value = color
+  changePenColor(color)
+}
+
 // 保存为png图片
-function saveAsPng(downloadName) {
+function saveImage(downloadName, type = 'png') {
   if (signaturePad.isEmpty()) {
     Toast('请先提供签名')
   } else {
-    const dataURL = signaturePad.toDataURL()
-    download(dataURL, `${downloadName || props.downloadName}.png`)
+    const dataURL = props.isTrimCanvas ? getTrimmedCanvas().toDataURL() : signaturePad.toDataURL()
+    if (type === 'png') {
+      download(dataURL, `${downloadName || props.downloadName}.png`)
+    } else {
+      download(dataURL, `${downloadName || props.downloadName}.jpg`)
+    }
   }
 }
 
+function saveAsPng(downloadName) {
+  saveImage(downloadName, 'png')
+}
+
 function saveAsJpg(downloadName) {
-  if (signaturePad.isEmpty()) {
-    Toast('请先提供签名')
-  } else {
-    const dataURL = signaturePad.toDataURL('image/jpeg')
-    download(dataURL, `${downloadName || props.downloadName}.jpg`)
-  }
+  saveImage(downloadName, 'jpg')
 }
 </script>
 
 <style lang="less" scoped>
 @import './../styles/common.less';
+@canvas-height: 250px;
 .signature-pad {
   background-color: #fff;
 }
+.signature-pad-header {
+  .display-flex-between();
+  padding: @padding-small;
+}
+.color-box,
+.handle-box {
+  .display-flex-center();
+}
+.color-item {
+  background-color: #fff;
+  border-radius: 50%;
+  padding: @padding-mini;
+  &.active {
+    background-color: #dde1e3;
+  }
+}
+.color-item-inner {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+}
+.color-item + .color-item {
+  margin-left: @margin-mini;
+}
+.handle-item {
+  box-sizing: border-box;
+  border: 1px solid #dde1e3;
+  border-radius: 50%;
+  .display-flex-center();
+  width: calc(30px + @padding-mini * 2);
+  height: calc(30px + @padding-mini * 2);
+  .handle-item-icon {
+    font-size: 20px;
+  }
+}
+.signature-pad-canvas-box {
+  box-sizing: border-box;
+  border: 1px solid #ddd;
+  font-size: 0;
+}
 .signature-pad-canvas {
-  height: 300px;
+  height: @canvas-height;
 }
 </style>
--- src/views/demo/signaturePad.vue
+++ src/views/demo/signaturePad.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="block" style="margin-left: 0;margin-right: 0;background: rgba(0,0,0,0);">
-    <div class="block-title van-hairline--bottom">签字板演示</div>
+  <div class="block" style="margin-left: 0;margin-right: 0;">
+    <div class="block-title van-hairline--bottom">签字板演示（不显示工具栏）</div>
     <div class="block-body" style="padding: 0;">
-      <signature-pad ref="signaturePad" downloadName="签名" style="border: 1px solid #ddd"></signature-pad>
-      <button @click="getSignature">获取签名</button>
-      <div>
-        <img v-if="canvasDataURL" :src="canvasDataURL" style="width: 100px">
+      <signature-pad downloadName="签名" :toolShow="false"></signature-pad>
+    </div>
+  </div>
+  <div class="block" style="margin-left: 0;margin-right: 0;background: rgba(0,0,0,0)">
+    <div class="block-title van-hairline--bottom" style="background: #fff;">签字板获取纯签名演示</div>
+    <div class="block-body" style="padding: 0;">
+      <signature-pad ref="signaturePad1" :isTrimCanvas="true" downloadName="签名"></signature-pad>
+      <div class="pdt-small pdl-middle pdr-middle pdb-mini">
+        <van-button type="success" @click="getIsEmpty1">画板是否为空</van-button>
+        <van-button type="default" @click="clear1">清空画板</van-button>
+        <van-button type="warning" @click="changeColor1">修改画笔颜色</van-button>
+      </div>
+      <div class="pdt-mini pdl-middle pdr-middle pdb-small">
+        <van-button type="danger" @click="download1">下载签名（png）</van-button>
+        <van-button type="primary" @click="getSignature1">获取纯签名</van-button>
+      </div>
+      <div class="pdl-middle pdr-middle">
+        <div class="pdt-small pdb-small">纯签名：</div>
+        <img v-if="canvasDataURL1" :src="canvasDataURL1" style="width: 100px">
+      </div>
+    </div>
+  </div>
+  <div class="block" style="margin-left: 0;margin-right: 0;background: rgba(0,0,0,0)">
+    <div class="block-title van-hairline--bottom" style="background: #fff;">签字板演示</div>
+    <div class="block-body" style="padding: 0;">
+      <signature-pad ref="signaturePad2" :colors="colors" :isTrimCanvas="false" downloadName="签名2"></signature-pad>
+      <div class="pdt-small pdl-middle pdr-middle pdb-mini">
+        <van-button type="success" @click="getIsEmpty2">画板是否为空</van-button>
+        <van-button type="default" @click="clear2">清空画板</van-button>
+        <van-button type="warning" @click="changeColor2">修改画笔颜色</van-button>
+      </div>
+      <div class="pdt-mini pdl-middle pdr-middle pdb-small">
+        <van-button type="danger" @click="download2">下载签名（png）</van-button>
+        <van-button type="primary" @click="getSignature2">获取签名</van-button>
+      </div>
+      <div class="pdl-middle pdr-middle">
+        <div class="pdt-small pdb-small">纯签名：</div>
+        <img v-if="canvasDataURL2" :src="canvasDataURL2" style="width: 100px">
       </div>
     </div>
   </div>
@@ -13,14 +47,59 @@
 
 <script setup>
 import { ref } from 'vue'
+import { Toast } from 'vant'
 import SignaturePad from './../../components/SignaturePad.vue'
 
-let signaturePad = ref(null)
-let canvasDataURL = ref('')
+let signaturePad1 = ref(null)
+let canvasDataURL1 = ref('')
+let signaturePad2 = ref(null)
+let canvasDataURL2 = ref('')
+const colors = ['#000', '#aaa', '#f00', '#0f0', '#00f']
 
-const getSignature = () => {
-  console.log(signaturePad.value.getTrimmedCanvas())
-  canvasDataURL.value = signaturePad.value.getTrimmedCanvas().toDataURL()
+const getIsEmpty1 = () => {
+  Toast(signaturePad1.value.isEmpty() ? '画板为空' : '画板不为空')
+}
+
+const clear1 = () => {
+  signaturePad1.value.clear()
+}
+
+const changeColor1 = () => {
+  signaturePad1.value.changePenColor(colors[(Math.random() * 1000).toFixed(0) * 1 % colors.length])
+}
+
+const download1 = () => {
+  signaturePad1.value.saveAsPng('纯签名')
+}
+
+const getSignature1 = () => {
+  const canvas = signaturePad1.value.getTrimmedCanvas()
+  if (canvas) {
+    canvasDataURL1.value = canvas.toDataURL()
+  }
+}
+
+const getIsEmpty2 = () => {
+  Toast(signaturePad2.value.isEmpty() ? '画板为空' : '画板不为空')
+}
+
+const clear2 = () => {
+  signaturePad2.value.clear()
+}
+
+const changeColor2 = () => {
+  signaturePad2.value.changePenColor(colors[(Math.random() * 1000).toFixed(0) * 1 % colors.length])
+}
+
+const download2 = () => {
+  signaturePad2.value.saveAsJpg()
+}
+
+const getSignature2 = () => {
+  const canvas = signaturePad2.value.getSignaturePad()
+  if (canvas) {
+    canvasDataURL2.value = canvas.toDataURL()
+  }
 }
 </script>
 
